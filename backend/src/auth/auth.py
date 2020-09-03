216446154670cf14b94c73698c593bc57218fb4b
Omneya El-Meligy <omneya.elmeligy@gmail.com>
2020-09-03 23:43:41 +0200

    fix

--- backend/src/auth/auth.py
+++ backend/src/auth/auth.py
@@ -7,7 +7,7 @@ from urllib.request import urlopen
 
 AUTH0_DOMAIN = 'dev-omn.us.auth0.com'
 ALGORITHMS = ['RS256']
-API_AUDIENCE = 'Coffee Shop'
+API_AUDIENCE = 'http://127.0.0.1:5000/drinks'
 
 ## AuthError Exception
 '''
